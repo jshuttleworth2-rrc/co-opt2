1f64db43e60ae5b1c927441533f544c63b70c0c7
Joshua Shuttleworth <jshuttleworth2@academic.rrc.ca>
2025-03-17 20:30:31 -0500

    Added drag and drop feature using Calvin-ll library

--- app/src/main/java/com/shuttle/co_opt2/MainActivity.kt
+++ app/src/main/java/com/shuttle/co_opt2/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.shuttle.co_opt2.ui.theme.Co_opt2Theme
+import sh.calvin.reorderable.ReorderableItem
+import sh.calvin.reorderable.rememberReorderableLazyListState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +32,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Co_opt2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CustomList()
+                    ReorderList()
                 }
             }
         }
@@ -31,17 +41,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CustomList() {
+    val itemList = List(25) { "Item $it" }
+    LazyColumn {
+        items(itemList) { item ->
+            Card( // Create a "card" around items, similar to MovieCards Demo
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp) // Create space between card items
+            ) {
+                Text(
+                    text = item,
+                    modifier = Modifier.padding(16.dp) // Add padding inside the card
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Co_opt2Theme {
-        Greeting("Android")
+fun ReorderList() {
+    val itemList = remember { mutableStateListOf(*List(25) { "Item $it"}.toTypedArray()) }
+    val lazyListState = rememberLazyListState()
+
+    val reorderableState = rememberReorderableLazyListState(lazyListState = lazyListState) { from, to ->
+            itemList.add(to.index, itemList.removeAt(from.index))
+    }
+
+    LazyColumn(
+        state = lazyListState,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        items(itemList, key = { it }) { item ->
+            ReorderableItem(reorderableState, key = item) { isDragging ->
+                Row {
+                    Card(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .let { if (isDragging) it else it }
+                    ) {
+                        Text(
+                            text = item,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+
+                    IconButton(
+                        modifier = Modifier.draggableHandle(),
+                        onClick = {},
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_snowflake_foreground),
+                            contentDescription = "Snowflake"
+                        )
+                    }
+                }
+            }
+        }
     }
 }
